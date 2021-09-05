6d7340d8c2c899fec7365c0e16842c499bd5e259
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-09-05 20:05:56 +0900

    add acontents add acontents

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter, Route, Link } from "react-router-dom";
 const PageOne = () => {
   return (
     <div>
-      架空請求サイトです
+      <h2>架空請求サイトを開いた時の感情になれるサイトです</h2>
       <Link to="/pagetwo">
         <p>黒ギャルレスリング</p>
         <p>黒ギャル相撲</p>
-        <p>黒ギャル柔道着パーティー</p>
+        <p>黒ギャル柔道</p>
+        <p>黒ギャル綱引き</p>
       </Link>
     </div>
   );
